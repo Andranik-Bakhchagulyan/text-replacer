c2bf92c55eba25afe433777bb9950ecaf1290920
Andranik-Bakhchagulyan <andranik.baxchagulyan@gmail.com>
2024-04-18 16:19:54 +0400

    first commit

---
+++ TextReplace/Class1.cs
@@ -0,0 +1,74 @@
+﻿using System.Text;
+
+namespace TextReplace;
+
+public class Class1
+{
+    public static void ReplaceAll(string filePath, string string1, string string2)
+    {
+        if (!File.Exists(filePath))
+        {
+            throw new FileNotFoundException("File not found.", filePath);
+        }
+
+        string tempFilePath = Path.GetTempFileName(); 
+
+        try
+        {
+            using (var reader = new StreamReader(filePath, Encoding.ASCII))
+            using (var writer = new StreamWriter(tempFilePath, false, Encoding.ASCII))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    line = line.Replace(string1, string2);
+                    writer.WriteLine(line);
+                }
+            }
+
+            File.Copy(tempFilePath, filePath, true);
+        }
+        finally
+        {
+            File.Delete(tempFilePath);
+        }
+    }
+
+    public static void ReplaceFirst(string filePath, string string1, string string2)
+    {
+        if (!File.Exists(filePath))
+        {
+            throw new FileNotFoundException("File not found.", filePath);
+        }
+
+        string tempFilePath = Path.GetTempFileName();
+
+        try
+        {
+            using (var reader = new StreamReader(filePath, Encoding.ASCII))
+            using (var writer = new StreamWriter(tempFilePath, false, Encoding.ASCII))
+            {
+                bool replaced = false;
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if (!replaced && line.Contains(string1))
+                    {
+                        line = line.Replace(string1, string2);
+                        replaced = true;
+                    }
+                    writer.WriteLine(line);
+                }
+            }
+
+            if (File.Exists(tempFilePath))
+            {
+                File.Copy(tempFilePath, filePath, true);
+            }
+        }
+        finally
+        {
+            File.Delete(tempFilePath);
+        }
+    }
+}
---
+++ TextReplace/Program.cs
@@ -0,0 +1,26 @@
+﻿using TextReplace;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        string filePath = "C://Users//andoo//Desktop//New Text Document.txt";
+        string string1 = "text"; 
+        string string2 = "text1"; 
+
+
+        try
+        {
+            Class1.ReplaceFirst(filePath, string1, string2);
+            Console.WriteLine("OK.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error Message: {ex.Message}");
+        }
+
+    }
+
+
+}
